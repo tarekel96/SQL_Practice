87b068d395314d76a634ed6a71cb8a5ae7b63f20
Tarek <elhaj102@mail.chapman.edu>
2023-10-12 11:42:21 -0700

    Saving progress - using the consumer complaints csv file to do more complex queries.

--- EmployeeSystem/queries_practice.sql
+++ EmployeeSystem/queries_practice.sql
@@ -124,3 +124,59 @@ SELECT e2.name as mngr_name
 FROM emp_cnt_dpt e1, Employee e2
 WHERE e1.cnt_dpt > 1
 AND e1.managerId = e2.id
+
+/*
+Based on the departments table, create an SQL table
+called employeeDepts that matches employees to departments.
+Assume (and enforce) that an employee can work in
+a single department. Don’t forget to add the right primary and foreign keys.
+*/
+-- first update employee table to have primary key
+ALTER TABLE employee ADD CONSTRAINT pk_employee PRIMARY KEY(id);
+
+CREATE TABLE employeeDepts (
+	employee_id INTEGER,
+	department_id INTEGER,
+	CONSTRAINT pk_employeeDepts PRIMARY KEY (employee_id), -- emp can work only in single department
+	FOREIGN KEY (employee_id) REFERENCES Employee(id),
+	FOREIGN KEY (department_id) REFERENCES departments(department_id)
+)
+
+/*
+Show SQL code to create a table to import the csv
+data in https://drive.google.com/file/d/1Uwpit1sCWyw0rKCYWepMy1wE1U_-lovN/
+
+Import the csv data in the previous problem into your database. Show your code.
+*/
+
+CREATE TABLE ConsumerComplaints (
+	DateReceived DATE,
+	ProductName VARCHAR(255),
+	Issue VARCHAR(255),
+	Company VARCHAR(200),
+	StateName CHAR(2),
+	ZipCode VARCHAR(5),
+	SubmittedVia VARCHAR(100),
+	DateSentToCompany DATE,
+	CompanyResponseToConsumer VARCHAR(100),
+	TimelyResponse VARCHAR(3),
+	ConsumerDisputed VARCHAR(3),
+	ComplaintID INTEGER
+)
+
+COPY ConsumerComplaints
+FROM '/Users/Tarek/Documents/UCI_MDS_Coding/cs224P/HW/1-Assignment/ConsumerComplaints.csv'
+DELIMITER ',' CSV HEADER;
+
+/*
+Write SQL query to show for each type of product name
+the average number of days between the date when a complaint
+is received and the date it is sent to the company, sorted
+by decreasing difference.
+*/
+
+-- practice query to understand DATE_PART fn
+SELECT 
+	DateReceived, DateSentToCompany, DateSentToCompany - DateReceived as NumOfDaysToProcess
+FROM ConsumerComplaints
+WHERE ComplaintID = 468882;
